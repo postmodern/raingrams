f308294bb88113c791333d4e1dcf9115febc67d9
Postmodern <postmodern.mod3@gmail.com>
2008-10-06 04:45:29 +0000

    * Added the NgramSet class.

---
+++ lib/raingrams/ngram_set.rb
@@ -0,0 +1,43 @@
+require 'raingrams/ngram'
+
+require 'set'
+
+module Raingrams
+  class NgramSet < Set
+
+    def select(&block)
+      selected_ngrams = self.class.new
+
+      each do |ngram|
+        selected_ngrams << ngram if block.call(ngram)
+      end
+
+      return selected_ngrams
+    end
+
+    def prefixed_by(prefix)
+      select { |ngram| ngram.prefixed_by?(prefix) }
+    end
+
+    def postfixed_by(postfix)
+      select { |ngram| ngram.postfixed_by?(postfix) }
+    end
+
+    def starts_with(gram)
+      select { |ngram| ngram.starts_with?(gram) }
+    end
+
+    def ends_with(gram)
+      select { |ngram| ngram.ends_with?(gram) }
+    end
+
+    def including(gram)
+      select { |ngram| ngram.include?(gram) }
+    end
+
+    def includes(*grams)
+      select { |ngram| ngram.includes?(*grams) }
+    end
+
+  end
+end
---
+++ spec/ngram_set_spec.rb
@@ -0,0 +1,54 @@
+require 'raingrams/ngram_set'
+
+require 'spec_helper'
+
+describe NgramSet do
+  before(:all) do
+    @ngrams = NgramSet[
+      Ngram[:the, :dog],
+      Ngram[:dog, :jumped],
+      Ngram[:jumped, :through],
+      Ngram[:through, :the],
+      Ngram[:the, :hoop]
+    ]
+  end
+
+  it "should select ngrams from the set" do
+    @ngrams.select { |ngram|
+      ngram.starts_with?(:the)
+    }.should == NgramSet[Ngram[:the, :dog], Ngram[:the, :hoop]]
+  end
+
+  it "should select ngrams with a specified prefixed" do
+    @ngrams.prefixed_by(Ngram[:dog]).should == NgramSet[
+      Ngram[:dog, :jumped]
+    ]
+  end
+
+  it "should select ngrams with a specified postfix" do
+    @ngrams.postfixed_by(Ngram[:through]).should == NgramSet[
+      Ngram[:jumped, :through]
+    ]
+  end
+
+  it "should select ngrams starting with a specified gram" do
+    @ngrams.starts_with(:jumped).should == NgramSet[Ngram[:jumped, :through]]
+  end
+
+  it "should select ngrams ending with a specified gram" do
+    @ngrams.ends_with(:dog).should == NgramSet[Ngram[:the, :dog]]
+  end
+
+  it "should select ngrams including a specified gram" do
+    @ngrams.including(:dog).should == NgramSet[
+      Ngram[:the, :dog],
+      Ngram[:dog, :jumped]
+    ]
+  end
+
+  it "should select ngrams which includes specified grams" do
+    @ngrams.includes(:the, :dog).should == NgramSet[
+      Ngram[:the, :dog],
+    ]
+  end
+end
