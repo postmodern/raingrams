a112b7782929fb6686f3230f4f841d3d6fc66a6f
Postmodern <postmodern.mod3@gmail.com>
2008-10-13 00:19:17 +0000

    * Updated spec test values.

--- spec/pentagram_model_spec.rb
+++ spec/pentagram_model_spec.rb
@@ -11,4 +11,91 @@ describe PentagramModel do
   end
 
   it_should_behave_like "Model"
+
+  it "should return ngrams from specified words" do
+    words = %w{Why is the Deliverator so equipped}
+    ngrams = [
+      Ngram[:Why, :is, :the, :Deliverator, :so],
+      Ngram[:is, :the, :Deliverator, :so, :equipped]
+    ]
+
+    @model.ngrams_from_words(words).should == ngrams
+  end
+
+  it "should return common ngrams from words" do
+    words = %w{The Deliverator is a future Archetype}
+    ngrams = []
+
+    @model.common_ngrams_from_words(words).should == ngrams
+  end
+
+  it "should return common ngrams from a specified fragment of text" do
+    fragment = %{The Deliverator is a future Archetype}
+    ngrams = []
+
+    @model.common_ngrams_from_fragment(fragment).should == ngrams
+  end
+
+  it "should return common ngrams from a specified sentence" do
+    sentence = %{The Deliverator is a future Archetype.}
+    ngrams = [
+      Ngram[Tokens.start, Tokens.start, Tokens.start, Tokens.start, Tokens.start],
+      Ngram[Tokens.start, Tokens.start, Tokens.start, Tokens.start, :The],
+      Ngram[Tokens.start, Tokens.start, Tokens.start, :The, :Deliverator],
+      Ngram[Tokens.start, Tokens.start, :The, :Deliverator, :is],
+      Ngram[Tokens.start, :The, :Deliverator, :is, :a],
+      Ngram[Tokens.stop, Tokens.stop, Tokens.stop, Tokens.stop, Tokens.stop]
+    ]
+
+    @model.common_ngrams_from_sentence(sentence).should == ngrams
+  end
+
+  it "should have a frequency for a specified ngram" do
+    ngram = Ngram[:it, :fires, :teensy, :darts, :that]
+
+    @model.frequency_of_ngram(ngram).should == 1
+  end
+
+  it "should have a probability for a specified ngram" do
+    ngram = Ngram[:it, :fires, :teensy, :darts, :that]
+
+    @model.probability_of_ngram(ngram).should == 1.0
+  end
+
+  it "should have a frequency for specified ngrams" do
+    ngrams = NgramSet[
+      Ngram[:but, :excess, :perspiration, :wafts, :through],
+      Ngram[:through, :a, :freshly, :napalmed, :forest],
+      Ngram[:the, :suit, :has, :sintered, :armorgel]
+    ]
+
+    @model.frequency_of_ngrams(ngrams).should == 3
+  end
+
+  it "should have a probability of specified ngrams" do
+    ngrams = NgramSet[
+      Ngram[:The, :Deliverator, :belongs, :to, :an],
+      Ngram[:Deliverator, :belongs, :to, :an, :elite]
+    ]
+
+    @model.probability_of_ngrams(ngrams).to_s.should == '1.0'
+  end
+
+  it "should have a probability for a specified fragment of text" do
+    fragment = %{The Deliverator belongs to an}
+
+    @model.fragment_probability(fragment).to_s.should == '1.0'
+  end
+
+  it "should have a probability for a specified sentence" do
+    sentence = %{So now he has this other job.}
+
+    @model.sentence_probability(sentence).to_s.should == '0.00117370892018779'
+  end
+
+  it "should have a probability for specified text" do
+    text = %{So now he has this other job. No brightness or creativity involved-but no cooperation either.}
+
+    @model.text_probability(text).to_s.should == '2.75518525865679e-06'
+  end
 end
--- spec/quadgram_model_spec.rb
+++ spec/quadgram_model_spec.rb
@@ -11,4 +11,96 @@ describe QuadgramModel do
   end
 
   it_should_behave_like "Model"
+
+  it "should return ngrams from specified words" do
+    words = %w{Why is the Deliverator so equipped}
+    ngrams = [
+      Ngram[:Why, :is, :the, :Deliverator],
+      Ngram[:is, :the, :Deliverator, :so],
+      Ngram[:the, :Deliverator, :so, :equipped]
+    ]
+
+    @model.ngrams_from_words(words).should == ngrams
+  end
+
+  it "should return common ngrams from words" do
+    words = %w{The Deliverator is a future Archetype}
+    ngrams = [
+      Ngram[:The, :Deliverator, :is, :a]
+    ]
+
+    @model.common_ngrams_from_words(words).should == ngrams
+  end
+
+  it "should return common ngrams from a specified fragment of text" do
+    fragment = %{The Deliverator is a future Archetype}
+    ngrams = [
+      Ngram[:The, :Deliverator, :is, :a]
+    ]
+
+    @model.common_ngrams_from_fragment(fragment).should == ngrams
+  end
+
+  it "should return common ngrams from a specified sentence" do
+    sentence = %{The Deliverator is a future Archetype.}
+    ngrams = [
+      Ngram[Tokens.start, Tokens.start, Tokens.start, Tokens.start],
+      Ngram[Tokens.start, Tokens.start, Tokens.start, :The],
+      Ngram[Tokens.start, Tokens.start, :The, :Deliverator],
+      Ngram[Tokens.start, :The, :Deliverator, :is],
+      Ngram[:The, :Deliverator, :is, :a],
+      Ngram[Tokens.stop, Tokens.stop, Tokens.stop, Tokens.stop]
+    ]
+
+    @model.common_ngrams_from_sentence(sentence).should == ngrams
+  end
+
+  it "should have a frequency for a specified ngram" do
+    ngram = Ngram[:it, :fires, :teensy, :darts]
+
+    @model.frequency_of_ngram(ngram).should == 1
+  end
+
+  it "should have a probability for a specified ngram" do
+    ngram = Ngram[:it, :fires, :teensy, :darts]
+
+    @model.probability_of_ngram(ngram).should == 1.0
+  end
+
+  it "should have a frequency for specified ngrams" do
+    ngrams = NgramSet[
+      Ngram[:but, :excess, :perspiration, :wafts],
+      Ngram[:a, :freshly, :napalmed, :forest],
+      Ngram[:suit, :has, :sintered, :armorgel]
+    ]
+
+    @model.frequency_of_ngrams(ngrams).should == 3
+  end
+
+  it "should have a probability of specified ngrams" do
+    ngrams = NgramSet[
+      Ngram[:The, :Deliverator, :belongs, :to],
+      Ngram[:Deliverator, :belongs, :to, :an]
+    ]
+
+    @model.probability_of_ngrams(ngrams).to_s.should == '1.0'
+  end
+
+  it "should have a probability for a specified fragment of text" do
+    fragment = %{The Deliverator belongs to}
+
+    @model.fragment_probability(fragment).to_s.should == '1.0'
+  end
+
+  it "should have a probability for a specified sentence" do
+    sentence = %{So now he has this other job.}
+
+    @model.sentence_probability(sentence).to_s.should == '0.00117370892018779'
+  end
+
+  it "should have a probability for specified text" do
+    text = %{So now he has this other job. No brightness or creativity involved-but no cooperation either.}
+
+    @model.text_probability(text).to_s.should == '2.75518525865679e-06'
+  end
 end
