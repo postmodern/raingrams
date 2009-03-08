8efbc9da07295372c372e927a20c9a82a3697f56
postmodern <postmodern.mod3@gmail.com>
2009-03-08 06:26:20 -0700

    Moved common_ngrams_from_* methods into the Commonality helper.

--- lib/raingrams/helpers/commonality.rb
+++ lib/raingrams/helpers/commonality.rb
@@ -8,6 +8,38 @@ module Raingrams
       end
 
       #
+      # Returns the ngrams which occur within the specified _words_ and
+      # within the model.
+      #
+      def common_ngrams_from_words(words)
+        ngrams_from_words(words).select { |ngram| has_ngram?(ngram) }
+      end
+
+      #
+      # Returns the ngrams which occur within the specified _fragment_ and
+      # within the model.
+      #
+      def common_ngrams_from_fragment(fragment)
+        ngrams_from_fragment(fragment).select { |ngram| has_ngram?(ngram) }
+      end
+
+      #
+      # Returns the ngrams which occur within the specified _sentence_ and
+      # within the model.
+      #
+      def common_ngrams_from_sentence(sentence)
+        ngrams_from_sentence(sentence).select { |ngram| has_ngram?(ngram) }
+      end
+
+      #
+      # Returns the ngrams which occur within the specified _text_ and
+      # within the model.
+      #
+      def common_ngrams_from_text(text)
+        ngrams_from_text(text).select { |ngram| has_ngram?(ngram) }
+      end
+
+      #
       # Returns the joint probability of the common ngrams between the
       # specified _fragment_ and the model.
       #
