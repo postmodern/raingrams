865fb976e96fc258c94f4193bb21bec24d69c548
postmodern <postmodern.mod3@gmail.com>
2009-04-23 21:27:09 -0700

    Added some documentation to Raingrams::Tokens.

--- lib/raingrams/tokens/tokens.rb
+++ lib/raingrams/tokens/tokens.rb
@@ -4,18 +4,30 @@ require 'raingrams/tokens/unknown'
 
 module Raingrams
   module Tokens
+    #
+    # Returns all defined tokens.
+    #
     def Tokens.all
       @@raingram_tokens ||= {}
     end
 
+    #
+    # Returns the start sentence token.
+    #
     def Tokens.start
       Tokens.all[:start] ||= StartSentence.new
     end
 
+    #
+    # Returns the stop sentence token.
+    #
     def Tokens.stop
       Tokens.all[:stop] ||= StopSentence.new
     end
 
+    #
+    # Returns the unknown word token.
+    #
     def Tokens.unknown
       Tokens.all[:unknown] ||= Unknown.new
     end
